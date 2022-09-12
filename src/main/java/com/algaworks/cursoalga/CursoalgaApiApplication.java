542a0568f860b9d133a0cccff29ad6ccdf57a0ac
luanmoraes21 <luan.moraees21@gmail.com>
2022-09-12 19:25:53 -0300

    Project Created

---
+++ src/main/java/com/algaworks/cursoalga/CursoalgaApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.cursoalga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoalgaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoalgaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/cursoalga/CursoalgaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.cursoalga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoalgaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
